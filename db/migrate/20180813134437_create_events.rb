48b373727e6b1025df59cd99bc36066622d7ecda
suke <suke083.tech@gmail.com>
2018-08-17 00:26:48 +0900

    change the column name to company_id from opelation_company_id

--- db/migrate/20180813134437_create_events.rb
+++ db/migrate/20180813134437_create_events.rb
@@ -2,7 +2,7 @@ class CreateEvents < ActiveRecord::Migration[5.2]
   def change
     create_table :events do |t|
       t.string :name, null: false
-      t.integer :operation_company_id, limit: 4
+      t.integer :company_id, limit: 4
       t.timestamps
     end
     add_index :events, :name, unique: true

75a810475edcf300ca2638687c20be1bcbda82f0
suke <suke083.tech@gmail.com>
2018-08-23 23:32:09 +0900

    Change logger to output by date

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -61,6 +61,7 @@ Rails.application.configure do
 
   # Suppress logger output for asset requests.
   config.assets.quiet = true
+  config.logger = ActiveSupport::Logger.new("log/development.log","daily")
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true

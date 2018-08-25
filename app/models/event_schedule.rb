3e3bf3b470e4e1627953b4f2808ce73d711eff70
suke <suke083.tech@gmail.com>
2018-08-25 16:34:06 +0900

    Remove EventSchedule#check_start_end

--- app/models/event_schedule.rb
+++ app/models/event_schedule.rb
@@ -4,9 +4,4 @@ class EventSchedule < ApplicationRecord
   validates :started_at, presence: true
   validates :ended_at, presence: true
   validates :address, length: { maximum: 255 }
-  validate :check_start_end
-
-  def check_start_end
-    errors.add('Please enter the date of the end date') unless self.started_at < self.ended_at
-  end
 end

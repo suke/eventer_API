a2c1ca399e8db285439151823f89858a0d709842
suke <suke083.tech@gmail.com>
2018-08-26 23:04:56 +0900

    Fix a typo

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,7 +2,7 @@ class Event < ApplicationRecord
   belongs_to :company, optional: true
   has_many :event_schedules
   validates :name, presence: true,
-                   uniqueness: true
+                   uniqueness: true,
                    length: { maximum: 255 }
   validates :company_id, presence: true,
                          length: { maximum: 11 }

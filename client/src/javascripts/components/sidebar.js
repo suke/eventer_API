a660c861e77cb4cb379f8366662074799983b65a
suke <suke083.tech@gmail.com>
2018-08-28 23:28:40 +0900

    Add sidebar component

--- client/src/javascripts/components/sidebar.js
+++ client/src/javascripts/components/sidebar.js
@@ -7,9 +7,9 @@ const Sidebar = ({ className }) => (
   <SidebarWrapper className={className}>
     <ul>
       {routes.map((route, index) => (
-        <li key={index}>
-          <Link to={route.path}>{route.name}</Link>
-        </li>
+        <ListItem key={index}>
+          <StyledLink to={route.path}>{route.name}</StyledLink>
+        </ListItem>
       ))}
     </ul>
     {routes.map((route, index) => (
@@ -20,6 +20,28 @@ const Sidebar = ({ className }) => (
 
 const SidebarWrapper = styled.div`
   height: 100vh;
+  padding: 20px 0;
+`
+
+const ListItem = styled.li`
+  border-bottom: 1px solid #666;
+  border-collapse: collapse;
+  transition: 0.2s;
+  &:hover {
+    background: #666;
+    color: white;
+  }
+`
+
+const StyledLink = styled(Link)`
+  display: inline-block;
+  padding: 10px;
+  width: 100%;
+  height: 100%;
+  transition: 0.2s;
+  &:hover {
+    color: white;
+  }
 `
 
 export default Sidebar

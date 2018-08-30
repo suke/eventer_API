ea049d5bfa7982a894063d8de5ea12ba1812abb8
suke <suke083.tech@gmail.com>
2018-08-30 23:10:02 +0900

    Use redux-logger

--- client/src/javascripts/store/configureStore.js
+++ client/src/javascripts/store/configureStore.js
@@ -1,12 +1,15 @@
 import { createStore, applyMiddleware } from 'redux'
 import createSagaMiddleware from 'redux-saga'
 import rootReducer from '../modules/rootReducer'
-// import logger from 'redux-logger'
+import logger from 'redux-logger'
 import rootSaga from '../sagas'
 export const sagaMiddleware = createSagaMiddleware()
 
 export default function configureStore() {
-  const store = createStore(rootReducer, applyMiddleware(sagaMiddleware))
+  const store = createStore(
+    rootReducer,
+    applyMiddleware(sagaMiddleware, logger)
+  )
 
   if (module.hot) {
     // Enable Webpack hot module replacement for reducers

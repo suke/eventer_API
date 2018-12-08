3ac36c99557b03a53e8711989334d6d3f686ab58
suke <suke083.tech@gmail.com>
2018-12-08 23:44:40 +0900

    add categorySagas

--- client/src/sagas/index.js
+++ client/src/sagas/index.js
@@ -1,6 +1,7 @@
 import { all } from 'redux-saga/effects'
 import eventSagas from './event'
 import companySagas from './company'
+import categorySagas from './category'
 export default function* rootSaga() {
-  yield all([...eventSagas, ...companySagas])
+  yield all([...eventSagas, ...companySagas, ...categorySagas])
 }

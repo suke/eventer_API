eb97beac71f1602371c0723a5a714a800f77dd8e
suke <suke083.tech@gmail.com>
2018-12-09 00:22:47 +0900

    create category form container

---
+++ client/src/containers/category/category_create_container.js
@@ -0,0 +1,20 @@
+import { connect } from 'react-redux'
+import { createCategory } from '../../modules/category'
+import CategoryCreate from '../../components/category/create'
+
+const mapStateToProps = state => {
+  return {}
+}
+
+const mapDispatchToPros = dispatch => {
+  return {
+    handleSubmit: (data, dispatch, { history }) => {
+      dispatch(createCategory({ data, history }))
+    }
+  }
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToPros
+)(CategoryCreate)

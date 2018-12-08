d69a7c521399adfd5d327ebc4d0a1242bab4adb3
suke <suke083.tech@gmail.com>
2018-12-08 23:45:07 +0900

    add categoryReducer

--- client/src/modules/rootReducer.js
+++ client/src/modules/rootReducer.js
@@ -2,10 +2,11 @@ import { combineReducers } from 'redux'
 import { reducer as formReducer } from 'redux-form'
 import eventReducer from '../modules/event'
 import companyReducer from '../modules/company'
-
+import categoryReducer from '../modules/category'
 const reducer = combineReducers({
   eventReducer,
   companyReducer,
+  categoryReducer,
   form: formReducer
 })
 

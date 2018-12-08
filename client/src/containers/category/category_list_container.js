43dc718750d8780e01dea0c42db54f5699619c2e
suke <suke083.tech@gmail.com>
2018-12-08 23:46:22 +0900

    add category container

---
+++ client/src/containers/category/category_list_container.js
@@ -0,0 +1,21 @@
+import { connect } from 'react-redux'
+import { fetchCategories, deleteCategory } from '../../modules/category'
+import CategoryList from '../../components/category/list'
+
+const mapStateToProps = state => {
+  return {
+    categories: state.categoryReducer.categories
+  }
+}
+
+const mapDispatchToPros = dispatch => {
+  return {
+    fetchCategories: () => dispatch(fetchCategories()),
+    deleteCategory: id => dispatch(deleteCategory(id))
+  }
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToPros
+)(CategoryList)

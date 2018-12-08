77e299067079da4c14297c275531f42c06a33080
suke <suke083.tech@gmail.com>
2018-12-09 00:22:27 +0900

    create category form

---
+++ client/src/components/category/form.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import styled from 'styled-components'
+import { Field } from 'redux-form'
+import { InputField } from '../form/field'
+import { SubmitButton } from '../../components/button'
+
+const CategoryForm = ({ handleSubmit }) => (
+  <Wrapper>
+    <form onSubmit={handleSubmit}>
+      <Field name="name" component={InputField} type="text" label="name" />
+      <StyledSubmitButton type="submit">Submit</StyledSubmitButton>
+    </form>
+  </Wrapper>
+)
+
+const Wrapper = styled.div`
+  padding: 20px;
+  margin: 0 auto;
+  max-width: 800px;
+  border: 1px solid #666;
+`
+
+const StyledSubmitButton = styled(SubmitButton)`
+  margin: 0 auto;
+`
+
+export default CategoryForm

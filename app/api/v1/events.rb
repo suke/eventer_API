e17033ff20450df12e692eec1780ebcbd464ac1c
suke <suke083.tech@gmail.com>
2018-12-09 09:35:28 +0900

    add event api

---
+++ app/api/v1/events.rb
@@ -0,0 +1,52 @@
+module V1
+  class Events < Grape::API
+    helpers V1::Helpers::Message
+    resources :events do
+      desc 'return events',
+          success: EventSerializer,
+          produces: %w[application/json]
+      get serializer: EventSerializer do
+        events = Event.all
+        events
+      end
+
+      desc 'create Event',
+          success: EventSerializer,
+          produces: %w[application/json]
+      params do
+        requires :name, type: String
+        optional :company_id, type: Integer
+      end
+      post serializer: EventSerializer do
+        declared_params = declared(params, include_missing: false)
+        event = Event.create!(declared_params)
+        event
+      end
+
+      resource ':id' do
+        desc 'update Event',
+          success: EventSerializer,
+          produces: %w[application/json]
+        params do
+          requires :id, type: Integer
+          requires :name, type: String
+          optional :company_id, type: Integer
+        end
+        put serializer: EventSerializer do
+          declared_params = declared(params, include_missing: false)
+          event = Event.find_by!(id: declared_params[:id])
+          event.update!(declared_params)
+          event
+        end
+        
+        desc 'delete Event'
+        delete do
+          id = params[:id].to_i
+          event = Event.find_by!(id: id)
+          event.destroy!
+          {id: id, message: success_delete_message('id', id)}
+        end
+      end
+    end
+  end
+end

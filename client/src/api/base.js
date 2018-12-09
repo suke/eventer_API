50b97dc2f0be21ea81e39713a7e43c060d622c2b
suke <suke083.tech@gmail.com>
2018-12-09 09:46:32 +0900

    change end point

--- client/src/api/base.js
+++ client/src/api/base.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 const Api = axios.create({
   xsrfHeaderName: 'X-CSRF-Token',
   withCredentials: true,
-  baseURL: 'http://localhost:3000'
+  baseURL: 'http://localhost:3000/api/v1'
 })
 
 export default Api

1bc102f843e58f3846b5f9ad84834f5bbe3b89a5
suke <suke083.tech@gmail.com>
2018-12-24 17:08:48 +0900

    use dayjs

--- client/src/components/form/date_picker.js
+++ client/src/components/form/date_picker.js
@@ -1,6 +1,10 @@
 import React from 'react'
 import DatePicker from 'react-datepicker'
+import dayjs from 'dayjs'
+import 'dayjs/locale/es'
+dayjs.locale('ja')
 
 export default ({ value, onChange }) => {
-  return <DatePicker selected={value || null} onChange={onChange} />
+  const date = dayjs(value).toDate()
+  return <DatePicker selected={date || null} onChange={onChange} />
 }

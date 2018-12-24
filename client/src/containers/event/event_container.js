e0a42062d10518b87bbde7fe06fd85498912ffa7
suke <suke083.tech@gmail.com>
2018-12-24 17:11:47 +0900

    fix use Reducer

--- client/src/containers/event/event_container.js
+++ client/src/containers/event/event_container.js
@@ -2,10 +2,10 @@ import { connect } from 'react-redux'
 import { fetchEventAndSchedule } from '../../modules/event'
 import Event from '../../components/event/index'
 
-const mapStateToProps = ({ eventReducer }) => {
+const mapStateToProps = ({ eventReducer, eventScheduleReducer }) => {
   return {
     events: eventReducer.events,
-    schedules: eventReducer.schedules
+    schedules: eventScheduleReducer.event_schedules
   }
 }
 

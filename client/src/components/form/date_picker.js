b31c757e5f75001aff097b4e70d94838c366ff66
suke <suke083.tech@gmail.com>
2018-12-24 17:28:30 +0900

    use timeSelect

--- client/src/components/form/date_picker.js
+++ client/src/components/form/date_picker.js
@@ -1,10 +1,27 @@
 import React from 'react'
 import DatePicker from 'react-datepicker'
+import styled from 'styled-components'
 import dayjs from 'dayjs'
 import 'dayjs/locale/es'
 dayjs.locale('ja')
 
 export default ({ value, onChange }) => {
   const date = dayjs(value).toDate()
-  return <DatePicker selected={date || null} onChange={onChange} />
+  return (
+    <StyledDatePicker
+      selected={date || null}
+      onChange={onChange}
+      dateFormat="YYYY/MM/dd hh:mm:ss"
+      showTimeSelect
+      timeIntervals={10}
+      timeCaption="time"
+      timeFormat="HH:mm"
+    />
+  )
 }
+
+const StyledDatePicker = styled(DatePicker)`
+  padding: 5px;
+  border-radius: 5px;
+  border: 1px solid #ccc;
+`

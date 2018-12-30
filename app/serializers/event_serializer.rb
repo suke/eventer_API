55ef1314ce2413becd191eec1f8ed27a1ede6fc0
suke <suke083.tech@gmail.com>
2018-12-30 22:15:35 +0900

    add site_url to serializers

--- app/serializers/event_serializer.rb
+++ app/serializers/event_serializer.rb
@@ -1,4 +1,4 @@
 class EventSerializer < ActiveModel::Serializer
-  attributes :id, :name, :company_id, :created_at, :updated_at
+  attributes :id, :name, :company_id, :site_url, :created_at, :updated_at
   belongs_to :company
 end

430fe4297b6f606117a1bef7fa39e0ab609ac84b
suke <suke083.tech@gmail.com>
2018-12-30 22:26:28 +0900

    fix initilaValues

--- client/src/containers/form/form_container.js
+++ client/src/containers/form/form_container.js
@@ -13,6 +13,7 @@ export const EditForm = connect(state => ({
   initialValues: {
     id: state.eventReducer.currentEvent.id,
     name: state.eventReducer.currentEvent.name,
-    company_id: state.eventReducer.currentEvent.company_id
+    company_id: state.eventReducer.currentEvent.company_id,
+    site_url: state.eventReducer.currentEvent.site_url
   }
 }))(DefaultForm)

25e7ab077385c5115d1c2a0c2689112487ca72c6
suke <suke083.tech@gmail.com>
2018-12-31 22:39:22 +0900

    add company spec

---
+++ spec/api/v1/company_spec.rb
@@ -0,0 +1,44 @@
+require 'rails_helper'
+
+describe V1::Companies do
+  describe 'GET /api/v1/companies' do
+    subject { get '/api/v1/companies' }
+    it 'returns success' do
+      subject
+      expect(response).to be_success
+    end
+  end
+
+  describe 'POST /api/v1/companies' do
+    subject { post '/api/v1/companies', params: { name: 'コミケット' } }
+    it 'returns success' do
+      subject
+      expect(response).to be_success
+    end
+    context 'params empty' do
+      subject { post '/api/v1/companies', params: {} }
+      it 'return 400' do
+        subject
+        expect(response.status).to eq(400)
+      end
+    end
+  end
+
+  describe 'PUT /api/v1/companies/:id' do
+    let(:company) { create(:company) }
+    subject { put "/api/v1/companies/#{company[:id]}", params: { name: 'クリエイション' } }
+    it 'return success' do
+      subject
+      expect(JSON.parse(response.body)['name']).to eq 'クリエイション'
+    end
+  end
+
+  describe 'DELETE /api/v1/companies/:id' do
+    let(:company) { create(:company) }
+    subject { delete "/api/v1/companies/#{company[:id]}" }
+    it 'return success' do
+      subject
+      expect(JSON.parse(response.body)['id']).to eq company[:id]
+    end
+  end
+end

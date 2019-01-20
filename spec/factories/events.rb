0f1c4b4be6cb8cbe1081c66af8aaad37a9692f21
suke <suke083.tech@gmail.com>
2019-01-20 22:13:05 +0900

    fix evetn factory

--- spec/factories/events.rb
+++ spec/factories/events.rb
@@ -14,7 +14,7 @@ FactoryBot.define do
   factory :event do
     sequence(:id) { |n| n }
     sequence(:name) { |n| "Event#{n}" }
-    sequence(:company_id) { |n| n }
+    sequence(:company_id) { create(:company).id }
     created_at { Time.zone.now }
     updated_at { Time.zone.now }
   end

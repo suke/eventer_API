492335b98420f4935a3222138f0a342ff5ba900c
suke <suke083.tech@gmail.com>
2019-01-20 23:49:46 +0900

    use grape-active_model_serializers

--- app/api/v1/root.rb
+++ app/api/v1/root.rb
@@ -2,6 +2,8 @@ module V1
   class Root < Grape::API
     version 'v1'
     format :json
+    formatter :json, Grape::Formatter::ActiveModelSerializers
+    error_formatter :json, Grape::Formatter::ActiveModelSerializers
 
     mount V1::Categories
     mount V1::Companies

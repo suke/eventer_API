543fb9c6faeb5c665a6c3b5d2fded21ecfaca34f
suke <suke083.tech@gmail.com>
2019-02-06 11:08:29 +0900

    use react-hooks

--- client/src/components/event/edit.js
+++ client/src/components/event/edit.js
@@ -1,39 +1,44 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { EventForm } from '../event/form'
 import Wrapper from '../main_wrapper'
 import { createOptions } from '../../util'
+import { useFirstMount } from '../../custom_fooks/index';
 
-class EventEdit extends Component {
-  componentDidMount() {
-    const {
-      currentEvent,
-      fetchCompanies,
-      fetchCategories,
-      events,
-      match
-    } = this.props
-    const event = events.find(event => {
-      return event.id === parseInt(match.params.id)
-    })
-    fetchCompanies()
-    fetchCategories()
-    currentEvent(event)
-  }
+function EventEdit(props) {
 
-  render() {
-    const { event, companies, categories, handleSubmit, history } = this.props
-    const options = createOptions(companies, 'id', 'name')
-    return (
-      <Wrapper>
-        <EventForm
-          history={history}
-          options={options}
-          handleSubmit={handleSubmit}
-          initialValues={event}
-        />
-      </Wrapper>
-    )
-  }
+  useFirstMount(props, (props) => {
+    if(isFirstRef.current) {
+      const {
+        currentEvent,
+        fetchCompanies,
+        fetchCategories,
+        events,
+        match
+      } = props
+      const event = events.find(event => {
+        return event.id === parseInt(match.params.id)
+      })
+      fetchCompanies()
+      fetchCategories()
+      currentEvent(event)
+      isFirstRef.current = false
+    }
+  })
+
+  const { event, companies, categories, handleSubmit, history } = props
+  const options = createOptions(companies, 'id', 'name')
+  const categoryOptions = createOptions(categories, 'id', 'name')
+  return (
+    <Wrapper>
+      <EventForm
+        history={history}
+        options={options}
+        categoryOptions={categoryOptions}
+        handleSubmit={handleSubmit}
+        initialValues={event}
+      />
+    </Wrapper>
+  )
 }
 
 export default EventEdit
---
+++ client/src/custom_fooks/index.js
@@ -0,0 +1,14 @@
+import { useEffect, useRef } from 'react';
+
+export function useFirstMount(currentState, callback) {
+  const isFirstMount = useRef(true);
+  const lastStateRet = useRef()
+
+  useEffect(() => {
+    if (isFirstMount.current) {
+      callback(currentState);
+      isFirstMount.current = false
+    }
+    lastStateRet.current = currentState
+  });
+}
